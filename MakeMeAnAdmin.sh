4cd0ccd7086626a9c63cd687c5a91e63a90336e9
Sten Villup <sten-erik.villup@transferwise.com>
2024-01-26 09:52:36 +0200

    test

--- MakeMeAnAdmin.sh
+++ MakeMeAnAdmin.sh
@@ -17,7 +17,7 @@
 #############################################
 # find the logged in user and let them know #
 #############################################
-Testing, testin 2
+Testing, testin 2342343
 currentUser=$(who | awk '/console/{print $1}')
 echo $currentUser
 
